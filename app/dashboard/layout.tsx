b3bf3831a84cd65bc45b0a1bebd973449582d8e6
devLeopar <oktarkara@gmail.com>
2025-03-11 22:22:08 +0300

    feat: first working version of chatbot

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,7 +13,6 @@ export default function DashboardLayout({
     <NavigationProvider>
       <div className="flex h-screen">
         <Authenticated>
-          <h1>SideBar</h1>
           <SideBar />
         </Authenticated>
 
